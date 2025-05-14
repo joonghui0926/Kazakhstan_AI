03205a98a7760e5b6f32baf4f76272aa4368fd77
joonghui0926 <jjoonghui@gmail.com>
2025-05-14 16:25:33 +0900

    Delete Session7_GPT2_TextGeneration.py

--- Session7_GPT2_TextGeneration.py
+++
@@ -1,6 +0,0 @@
-# Session 7: GPT-2 Text Generation
-from transformers import pipeline
-generator = pipeline('text-generation', model='gpt2')
-prompt = "The future of AI is"
-outputs = generator(prompt, max_length=50, num_return_sequences=1)
-print(outputs[0]['generated_text'])
